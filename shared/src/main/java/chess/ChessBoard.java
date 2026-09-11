af2711898e374775299feb8b4a0abd883706bffe
Ben Finch <295913068+benfwork@users.noreply.github.com>
2026-09-11 17:28:15 -0600

    add reset board method logic

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -34,11 +34,49 @@ public class ChessBoard {
         return squares[position.getRow()-1][position.getColumn()-1];
     }
 
+    /* takes a row and column and the default piece type for a new board */
+    public ChessPiece.PieceType getStartingPieceType(int row, int col){
+        if (row == 1 | row == 8){
+            /* rooks */
+            if (col == 1 | col == 8){
+                return ChessPiece.PieceType.ROOK;
+            }
+            if (col == 2 | col == 7){
+                return ChessPiece.PieceType.KNIGHT;
+            }
+            if (col == 3 | col == 6){
+                return ChessPiece.PieceType.BISHOP;
+            }
+            if (col == 4){
+                return ChessPiece.PieceType.QUEEN;
+            }
+            if (col == 5){
+                return ChessPiece.PieceType.KING;
+            }
+        }
+        else if (row == 2 | row == 7) {
+            return ChessPiece.PieceType.PAWN;
+        }
+        return null;
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        /*  row */
+        for (int row = 1; row < 9; row ++){
+            for (int col = 1; col < 9; col++){
+                ChessPiece.PieceType chessPieceType = getStartingPieceType(row, col);
+                /* do not attempt to add a piece where there shouldn't be one */
+                if (chessPieceType != null){
+                    ChessGame.TeamColor teamColor = col < 5 ? ChessGame.TeamColor.WHITE : ChessGame.TeamColor.BLACK;
+                    ChessPosition chessPosition = new ChessPosition(row, col);
+                    ChessPiece newPiece = new ChessPiece(teamColor, chessPieceType);
+                    addPiece(chessPosition, newPiece);
+                }
+            }
+        }
     }
 }
