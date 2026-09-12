e30a4788ee2b620100131add42f383a4c9ef3514
Ben Finch <295913068+benfwork@users.noreply.github.com>
2026-09-11 19:16:52 -0600

    implementing(p0): king moves handler

--- shared/src/main/java/chess/MoveHandler/ChessMoveHandler.java
+++ shared/src/main/java/chess/MoveHandler/ChessMoveHandler.java
@@ -3,13 +3,23 @@ package chess.MoveHandler;
 import chess.ChessBoard;
 import chess.ChessPiece;
 import chess.ChessPosition;
-import chess.ChessMove
+import chess.ChessMove;
 
 import java.util.HashSet;
 
 
-public interface ChessMoveHandler {
-    static HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition position){
+public class ChessMoveHandler {
+    public final ChessPosition position;
+    public final ChessBoard board;
+    public final ChessPiece piece;
+
+    public ChessMoveHandler(ChessPosition position, ChessBoard board, ChessPiece piece) {
+        this.position = position;
+        this.board = board;
+        this.piece = piece;
+    }
+
+    HashSet<ChessMove> getMoves(){
         return null;
     }
 
@@ -17,5 +27,11 @@ public interface ChessMoveHandler {
         int row = square.getRow();
         int col = square.getColumn();
         return (1 <= row && row <= 8 && 1 <= col && col <= 8);
+    }
+
+    boolean isNullOrOtherTeam(ChessPosition position){
+        boolean isNull = (board.getPiece(position) == null);
+        boolean isOtherTeam = (board.getPiece(position).getTeamColor() != piece.getTeamColor());
+        return isNull | isOtherTeam;
     }
 }
--- shared/src/main/java/chess/MoveHandler/KingMovesHandler.java
+++ shared/src/main/java/chess/MoveHandler/KingMovesHandler.java
@@ -1,4 +1,34 @@
 package chess.MoveHandler;
 
-public class KingMovesHandler {
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.HashSet;
+
+public class KingMovesHandler extends ChessMoveHandler{
+    public KingMovesHandler(ChessPosition position, ChessBoard board, ChessPiece piece) {
+        super(position, board, piece);
+    }
+
+    public HashSet<ChessMove> getMoves(){
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        HashSet<ChessMove> validMoves = HashSet.newHashSet(8);
+
+        int[][] relativeMoves = {{-1, 1}, {0, 1}, {1, 1}, {-1, 0}, {1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+        for (int[] move : relativeMoves){
+            int x = move[0];
+            int y = move[1];
+            int newRow = row + x;
+            int newCol = col + y;
+            ChessPosition newPosition = new ChessPosition(newRow, newCol)
+            if (isValidSquare(newPosition) && isNullOrOtherTeam(newPosition)){
+                validMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+
+    }
 }
