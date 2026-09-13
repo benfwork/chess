d8cb4665c1de7ab68c79581ef19186ea16f89b6a
Ben Finch <295913068+benfwork@users.noreply.github.com>
2026-09-12 20:09:32 -0600

    (p0) final! implemented pawn promotion logic and fixed diagonal bug. pawn tests pass!!

--- shared/src/main/java/chess/moveHandler/PawnMoveHandler.java
+++ shared/src/main/java/chess/moveHandler/PawnMoveHandler.java
@@ -28,6 +28,32 @@ public class PawnMoveHandler extends ChessMoveHandler {
         return validMoves;
     }
 
+    public final HashSet<ChessMove> addAllPromoPieces(ChessPosition startPos, ChessPosition endPos){
+        HashSet<ChessMove> moves = new HashSet<>();
+        moves.add(new ChessMove(startPos, endPos, ChessPiece.PieceType.ROOK));
+        moves.add(new ChessMove(startPos, endPos, ChessPiece.PieceType.QUEEN));
+        moves.add(new ChessMove(startPos, endPos, ChessPiece.PieceType.BISHOP));
+        moves.add(new ChessMove(startPos, endPos, ChessPiece.PieceType.KNIGHT));
+        return moves;
+    }
+
+    // should only be invoked for moves already determined to be valid
+    public final HashSet<ChessMove> determinePromotionPiece(ChessPosition startPos, ChessPosition endPos){
+        HashSet<ChessMove> moves = new HashSet<>();
+        // if pawn is in promotion row, add all items to hashset
+        if ((endPos.getRow() == 8) && piece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            return (addAllPromoPieces(startPos, endPos));
+        }
+        else if ((endPos.getRow() == 1) && piece.getTeamColor() == ChessGame.TeamColor.BLACK){
+            return (addAllPromoPieces(startPos, endPos));
+        }
+        else {
+            moves.add(new ChessMove(startPos, endPos, null));
+            return moves;
+        }
+    }
+
+
     @Override
     public HashSet<ChessMove> getMoves() {
         var pieceColor = piece.getTeamColor();
@@ -42,14 +68,14 @@ public class PawnMoveHandler extends ChessMoveHandler {
         ChessPosition oneForward = new ChessPosition(row + pawnDirection, col);
         boolean oneForwardIsValid = isValidSquare(oneForward) && board.getPiece(oneForward) == null;
         if (oneForwardIsValid){
-            validMoves.add(new ChessMove(position, oneForward, null));
+            validMoves.addAll(determinePromotionPiece(position, oneForward));
         }
 
         // if the pawn is at the starting place, check two in front
-        if (row == pawnStartRow && oneForwardIsValid){
+        if (row == pawnStartRow && oneForwardIsValid) {
             ChessPosition twoForward = new ChessPosition(row + (pawnDirection * 2), col);
-            if (isValidSquare(twoForward) && board.getPiece(twoForward) == null){
-                validMoves.add(new ChessMove(position, oneForward, null));
+            if (isValidSquare(twoForward) && board.getPiece(twoForward) == null) {
+                validMoves.addAll(determinePromotionPiece(position, twoForward));
             }
         }
 
@@ -58,10 +84,10 @@ public class PawnMoveHandler extends ChessMoveHandler {
         ChessPosition diagonalRight = new ChessPosition(row + pawnDirection, col + 1);
 
         if (isValidSquare(diagonalRight) && board.getPiece(diagonalRight) != null && board.getPiece(diagonalRight).getTeamColor() != pieceColor){
-            validMoves.add(new ChessMove(position, diagonalRight, null));
+            validMoves.addAll(determinePromotionPiece(position, diagonalRight));
         }
-        else if (isValidSquare(diagonalLeft) && board.getPiece(diagonalLeft) != null && board.getPiece(diagonalLeft).getTeamColor() != pieceColor){
-            validMoves.add(new ChessMove(position, diagonalLeft, null));
+        if (isValidSquare(diagonalLeft) && board.getPiece(diagonalLeft) != null && board.getPiece(diagonalLeft).getTeamColor() != pieceColor){
+            validMoves.addAll(determinePromotionPiece(position, diagonalLeft));
         }
 
         return validMoves;
